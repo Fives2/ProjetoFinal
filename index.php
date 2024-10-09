8504f38501ecbc3cb66ed064f07535aaef488c1d
Fives2 <gamersbr.bolo@gmail.com>
2024-10-09 20:44:09 -0300

    initial import commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="CSS/nav.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fruto&Cia</title>
+
+<?php
+
+session_start();
+
+include "page/navbar.php";
+
+
+
+// include "page/footer";
+?>
+
+</head>
+<body>
+
+
+</body>
+</html>
---
+++ page/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar">
+        <div class="navbar-container">
+            <!-- Home -->
+            <div class="home">
+                <a href="index.php">
+                    <img src="IMG/Fruto&Cia.png">
+                </a>
+            </div>
+            
+            <!-- Centro -->
+            <div class="center-text">
+                <h1>Saúde e Frescura</h1>
+            </div>
+
+            <!-- Links -->
+            <ul class="menu-right">
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Sobre Nós</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#"><img src="IMG/carrinho.png" 
+                style="width: 25px; height: 25px;"></a></li>
+            </ul>
+        </div>
+    </nav>
