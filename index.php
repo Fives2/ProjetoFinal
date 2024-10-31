b04df1d4819d2a41238f869322e51a8dbecf661a
Fives2 <gamersbr.bolo@gmail.com>
2024-10-31 20:26:44 -0300

    favicon

--- index.php
+++ index.php
@@ -6,24 +6,36 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="css/home.css">
     <link rel="stylesheet" href="css/nav.css">
     <link rel="stylesheet" href="css/footer.css">
-
-
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,
+     initial-scale=1.0">
     <title>Fruto&Cia</title>
+    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
 
 </head>
 <body>
 
 <?php
-
-
 include "page/navbar.php";
 
+?>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
 
-
+<?php
 include "page/footer.php";
 ?>
 
