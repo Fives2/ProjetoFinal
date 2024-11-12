a53679914777edc65d909a989010b38bb27bf23d
Fives2 <gamersbr.bolo@gmail.com>
2024-11-11 21:17:13 -0300

    ccs

--- index.php
+++ index.php
@@ -13,31 +13,32 @@ session_start();
     <meta name="viewport" content="width=device-width,
      initial-scale=1.0">
     <title>Fruto&Cia</title>
-    <link rel="icon" type="image/x-icon" href="/img/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="css/img/favicon.ico">
 
 </head>
 <body>
 
 <?php
 include "page/navbar.php";
-
 ?>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+
+<div class="frutoso">
+a
+b
+c
+f
+</div>
 
 <?php
 include "page/footer.php";
 ?>
+
+<style>
+
+
+
+</style>
+
 
 </body>
 </html>
